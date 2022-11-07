5f35af1e52dc7eb9f59d59b8fd5b0536d43069cc
YongGit <1013588891@qq.com>
2022-11-07 08:46:05 +0800

    auto

--- hooks/assets/chunks/throttle.1274227c.js
+++
@@ -1,1 +0,0 @@
-import{d as n}from"./debounce.1d57d7c3.js";import{b as l}from"./screenfull.b8389cbf.js";var d=n,g=l,c="Expected a function";function f(i,a,r){var e=!0,t=!0;if(typeof i!="function")throw new TypeError(c);return g(r)&&(e="leading"in r?!!r.leading:e,t="trailing"in r?!!r.trailing:t),d(i,a,{leading:e,maxWait:a,trailing:t})}var b=f;export{b as t};
---
+++ hooks/assets/chunks/throttle.8a997959.js
@@ -0,0 +1,1 @@
+import{d as n}from"./debounce.802f8dfe.js";import{b as l}from"./screenfull.bc33cc94.js";var d=n,g=l,c="Expected a function";function f(i,a,r){var e=!0,t=!0;if(typeof i!="function")throw new TypeError(c);return g(r)&&(e="leading"in r?!!r.leading:e,t="trailing"in r?!!r.trailing:t),d(i,a,{leading:e,maxWait:a,trailing:t})}var b=f;export{b as t};
