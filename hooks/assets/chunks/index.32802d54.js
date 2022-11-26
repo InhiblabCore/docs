3ede2d36e500f49405a4363ac1a38fe7f07fe3a7
YongGit <1013588891@qq.com>
2022-11-26 10:39:06 +0800

    auto

--- hooks/assets/chunks/index.32802d54.js
+++
@@ -1,1 +0,0 @@
-import{t as n}from"./throttle.04a5f1d9.js";import{r as f,s,k as m}from"../app.34efe815.js";function v(a,e){var r;const u=f(a),l=(r=e==null?void 0:e.wait)!=null?r:1e3,t=s(()=>n((...c)=>u.value([...c]),l,e));return m(()=>{t.value.cancel()}),{run:t,cancel:t.value.cancel,flush:t.value.flush}}export{v as u};
