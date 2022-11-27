a02e81b537ae92444453e553a926cefba985f6d7
YongGit <1013588891@qq.com>
2022-11-27 19:04:31 +0800

    auto

--- hooks/assets/chunks/index.912d1635.js
+++
@@ -1,1 +0,0 @@
-import{r as i,l as f}from"../app.58f63346.js";function m(c,a,d){const n=c.map(e=>window.matchMedia(e)),r=()=>{const e=n.findIndex(t=>t.matches);return typeof a[e]!="undefined"?a[e]:d},o=i(r()),s=()=>{o.value=r()};return f(e=>{n.forEach(t=>{t.addListener(s)}),e(()=>{n.forEach(t=>t.removeListener(s))})}),o}export{m as u};
---
+++ hooks/assets/chunks/index.d2557b48.js
@@ -0,0 +1,1 @@
+import{b as o,H as s,I as t}from"../app.3f3e34b8.js";function r(n=()=>{}){const e=()=>{t(()=>{n()})};return o(()=>{window.addEventListener("resize",e,!1)}),s(()=>{window.removeEventListener("resize",e)}),null}export{r as u};
