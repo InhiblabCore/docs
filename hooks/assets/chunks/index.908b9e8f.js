39dbe08ff5870468d1a4a28b3d4bfbef407e5ad6
YongGit <1013588891@qq.com>
2022-12-01 06:50:21 +0800

    auto

--- hooks/assets/chunks/index.908b9e8f.js
+++
@@ -1,1 +0,0 @@
-import{b as o,H as s,I as t}from"../app.074a1445.js";function r(n=()=>{}){const e=()=>{t(()=>{n()})};return o(()=>{window.addEventListener("resize",e,!1)}),s(()=>{window.removeEventListener("resize",e)}),null}export{r as u};
