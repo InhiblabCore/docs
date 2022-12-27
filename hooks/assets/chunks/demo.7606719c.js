a16e1263ea71e0133006bb0e9090a331e40a37b1
YongGit <1013588891@qq.com>
2022-12-27 11:58:51 +0800

    auto

---
+++ hooks/assets/chunks/demo.7606719c.js
@@ -0,0 +1,1 @@
+import"./stringify.2d2283aa.js";import"./intersection-observer.38c1bba6.js";import{u as o}from"./index.a3c0bc69.js";import{e as t,r as n,o as r,c as s,t as a}from"../app.6bc27469.js";const f=t({__name:"demo",setup(c){const e=n(0);return o(()=>e.value+=1),(i,m)=>(r(),s("div",null,"Windows change count\uFF1A "+a(e.value),1))}});export{f as _};
---
+++ hooks/assets/chunks/index.c72f5da9.js
@@ -0,0 +1,1 @@
+import{t as n}from"./throttle.ed3887a3.js";import{r as f,s,k as m}from"../app.6bc27469.js";function v(a,e){var r;const u=f(a),l=(r=e==null?void 0:e.wait)!=null?r:1e3,t=s(()=>n((...c)=>u.value([...c]),l,e));return m(()=>{t.value.cancel()}),{run:t,cancel:t.value.cancel,flush:t.value.flush}}export{v as u};
