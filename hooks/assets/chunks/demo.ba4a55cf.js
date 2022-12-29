14717a9e957361facc6a8eba2426e6fd43ba293e
YongGit <1013588891@qq.com>
2022-12-29 11:56:00 +0800

    auto

--- hooks/assets/chunks/demo.ba4a55cf.js
+++
@@ -1,1 +0,0 @@
-import"./stringify.be61d207.js";import"./intersection-observer.38c1bba6.js";import{u as o}from"./index.20657f22.js";import{e as t,r as n,o as r,c as s,t as a}from"../app.3e3db97d.js";const f=t({__name:"demo",setup(c){const e=n(0);return o(()=>e.value+=1),(i,m)=>(r(),s("div",null,"Windows change count\uFF1A "+a(e.value),1))}});export{f as _};
