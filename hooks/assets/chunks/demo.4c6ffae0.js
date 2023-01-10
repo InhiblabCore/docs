3dc33d9dbdcb6880b2fd7e4e7d4a9d37f64d4fa8
YongGit <1013588891@qq.com>
2023-01-10 18:01:01 +0800

    auto

--- hooks/assets/chunks/demo.4c6ffae0.js
+++
@@ -1,1 +0,0 @@
-import"./stringify.dc8c2ebb.js";import"./intersection-observer.38c1bba6.js";import{u as o}from"./index.a5f8ec2b.js";import{e as t,r,o as a,c as s,t as n}from"../app.a1bd5cfe.js";const f=t({__name:"demo",setup(m){const e=r(0);return o(()=>{e.value+=1},2e3),(p,c)=>(a(),s("div",null,n(e.value),1))}});export{f as _};
---
+++ hooks/assets/chunks/demo.efeda5f2.js
@@ -0,0 +1,1 @@
+import"./stringify.dc8c2ebb.js";import"./intersection-observer.38c1bba6.js";import{u as o}from"./index.4422512a.js";import{e as t,r as a,o as r,c as s,t as m}from"../app.a1bd5cfe.js";const f=t({__name:"demo",setup(n){const e=a(0);return o(()=>{e.value+=1},1e3,{immediate:!1}),(i,p)=>(r(),s("div",null,m(e.value),1))}});export{f as _};
