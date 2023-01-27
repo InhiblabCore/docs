ceed1f44055679b782a36303d6b3112d315960a9
YongGit <1013588891@qq.com>
2023-01-27 09:53:33 +0800

    auto

--- hooks/assets/chunks/demo.b1aa97a5.js
+++
@@ -1,1 +0,0 @@
-import"./stringify.2f6e4c4d.js";import{u as o}from"./index.75ef1849.js";import"./intersection-observer.38c1bba6.js";import{e as t,r,o as a,c as n,t as s}from"../app.9c682a42.js";const f=t({__name:"demo",setup(m){const e=r(0);return o(()=>{e.value+=1},2e3),(p,c)=>(a(),n("div",null,s(e.value),1))}});export{f as _};
