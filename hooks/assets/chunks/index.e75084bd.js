7daebe1c11b9657ff39dfd7cb249f86b7224c635
YongGit <1013588891@qq.com>
2023-02-09 11:31:38 +0800

    auto

---
+++ hooks/assets/chunks/index.e75084bd.js
@@ -0,0 +1,1 @@
+const o=n=>typeof n=="function",t=n=>typeof n=="number";export{o as a,t as i};
---
+++ hooks/assets/chunks/isBrowser.dc916034.js
@@ -0,0 +1,1 @@
+const e=!!(typeof window!="undefined"&&window.document&&window.document.createElement);export{e as i};
