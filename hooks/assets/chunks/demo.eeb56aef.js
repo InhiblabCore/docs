ce001d600191a87f88ee1b63033ac0605079f455
YongGit <1013588891@qq.com>
2023-02-15 10:38:04 +0800

    auto

---
+++ hooks/assets/chunks/demo.eeb56aef.js
@@ -0,0 +1,1 @@
+import"./stringify.60d04010.js";import{u as o}from"./index.96637fc4.js";import"./intersection-observer.38c1bba6.js";import{e as t,r,o as a,c as n,t as s}from"../app.70c79ca7.js";const f=t({__name:"demo",setup(m){const e=r(0);return o(()=>{e.value+=1},2e3),(p,c)=>(a(),n("div",null,s(e.value),1))}});export{f as _};
---
+++ hooks/assets/chunks/index.e46e619a.js
@@ -0,0 +1,1 @@
+import{t as n}from"./throttle.9c552b2d.js";import{r as f,s,k as m}from"../app.70c79ca7.js";function v(a,e){var r;const u=f(a),l=(r=e==null?void 0:e.wait)!=null?r:1e3,t=s(()=>n((...c)=>u.value([...c]),l,e));return m(()=>{t.value.cancel()}),{run:t,cancel:t.value.cancel,flush:t.value.flush}}export{v as u};
