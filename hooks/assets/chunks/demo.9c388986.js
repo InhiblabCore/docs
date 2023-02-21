2f4884087c8cb7f487db6e960e3effd3cc9f60d1
YongGit <1013588891@qq.com>
2023-02-21 11:19:44 +0800

    auto

--- hooks/assets/chunks/demo.9c388986.js
+++
@@ -1,1 +0,0 @@
-import"./stringify.60d04010.js";import"./intersection-observer.38c1bba6.js";import{u as o}from"./index.1a278b84.js";import{e as t,r as n,o as r,c as s,t as a}from"../app.70c79ca7.js";const f=t({__name:"demo",setup(c){const e=n(0);return o(()=>e.value+=1),(i,m)=>(r(),s("div",null,"Windows change count\uFF1A "+a(e.value),1))}});export{f as _};
