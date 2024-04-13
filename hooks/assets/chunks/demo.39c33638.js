2c6372ab3a298054df1eda19b84995989135cc92
YongGit <1013588891@qq.com>
2024-04-13 16:18:07 +0800

    auto

--- hooks/assets/chunks/demo.39c33638.js
+++
@@ -1,1 +0,0 @@
-import{e as a,r as t,Q as n,o,c as s,t as r}from"../app.e9afd670.js";const u=a({__name:"demo",setup(c){const e=t(0);return n(()=>{e.value+=1},2e3),(l,_)=>(o(),s("div",null,r(e.value),1))}});export{u as _};
