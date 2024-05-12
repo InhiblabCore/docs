128e525d527cce2da35bd6425342f7bad5227674
YongGit <1013588891@qq.com>
2024-05-12 20:55:26 +0800

    auto

--- hooks/assets/chunks/demo.fea21403.js
+++
@@ -1,1 +0,0 @@
-import{e as n,r as o,au as a,o as s,c as t,t as c}from"../app.e653f72a.js";const l=n({__name:"demo",setup(r){const e=o(0);return a(()=>e.value+=1),(u,i)=>(s(),t("div",null,"Windows change count\uFF1A "+c(e.value),1))}});export{l as _};
