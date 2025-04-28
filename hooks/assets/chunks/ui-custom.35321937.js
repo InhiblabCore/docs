1e0d081bc7714abfe598f946087263a653984edf
YongGit <1013588891@qq.com>
2025-04-28 12:15:57 +0800

    feat: un

--- hooks/assets/chunks/ui-custom.35321937.js
+++ hooks/assets/chunks/ui-custom.35321937.js
@@ -44,9 +44,4 @@ const A = r({
   },
 });
 
-alert(
-  "该网站已经不再维护，将跳转到新的 2.0 文档网站以此获得更好的体验 / This website has been shut down. Please jump to the new 2.0 documentation website for better experience."
-);
-window.location.href = "https://inhiblabcore.github.io/vue-hooks-plus";
-
 export { A as default };
